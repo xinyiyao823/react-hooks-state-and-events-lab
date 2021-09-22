01c989ec43accd0e6ba607d37f9bc41ff3a01d29
+ <+>
2021-09-22 17:07:46 -0500

    commit tree 71cc18ecff9e470dcc1d56a736967fd3cbef14ad

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
+const [isLight, setIsLight] = useState(false)
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isLight ? "App dark" : "App light"
+
+  function handleLight(isLight) {
+      setIsLight((isLight) => !isLight)
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleLight}>{isLight ? "Dark Mode" : "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [addItem, setAddItem] = useState(true) // true for add
+  const itemClass = addItem ? "" : "in-cart"
+  
+  function handleCart(addItem) {
+      setAddItem((addItem) => !addItem)
+      
+  }
+
+
   return (
-    <li className="">
+    //make ternary conditional for class attribute
+    <li className={itemClass}> 
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>{addItem ? "Add to Cart" : "Remove from Cart"}</button>
     </li>
   );
 }
