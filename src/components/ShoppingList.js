d3ac9d7266eed734447522566e4a9433ba14b9ee
+ <+>
2021-09-22 17:08:03 -0500

    completed assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('All'); //false for when nothing is selected in dropdown
+  
+  function handleSelect(e) {
+    setSelectedCategory(e.target.value) // produce, dairy, dessert
+    
+      
+  }
+
+   items.filter((item) => item.category === selectedCategory ? <li>{item}</li> : null ) // if item's category doesn't equal value, remove those items
+  const food = (selectedCategory === "All") ? items : items.filter((item) => item.category === selectedCategory)
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleSelect} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +24,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {food.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
